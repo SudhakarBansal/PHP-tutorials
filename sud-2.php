45bdcea754495e06f0cc2c16f5193fabbd6c72a8
Sudhakar <sidhu1171bansal@gmail.com>
2023-08-11 11:57:41 +0530

    Using PHP in HTML

---
+++ sud-2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php with HTML</title>
+</head>
+<body>
+    <h1>I am using Php with HTML.</h1>
+    <?php
+    echo"Hello world";
+    ?>
+</body>
+</html>
