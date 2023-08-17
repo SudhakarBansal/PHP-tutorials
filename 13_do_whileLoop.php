da586ff9c0d750ba7f5a2be15c0f5575def31c0f
Sudhakar <sidhu1171bansal@gmail.com>
2023-08-17 17:11:22 +0530

    Do While loop

---
+++ 13_do_whileLoop.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>If else if else</title>
+</head>
+<body>
+    <h1>While loop in php</h1>
+    <?php 
+        $a = 1;
+        do{
+            echo "$a<br>";  
+            $a++;             
+        }
+        while ($a <= 5) ;
+    ?>
+</body>
+</html>
