3ef6d1850ea8f10f3c92ec920be523a073fd91fd
Sudhakar <sidhu1171bansal@gmail.com>
2023-08-17 17:03:43 +0530

    Ternary Operator

---
+++ 11_Ternary_operator.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>If else if else</title>
+</head>
+<body>
+    <h1>If else if else statement in php</h1>
+    <?php 
+        $a = 5;
+        $b = 10;
+        echo ($a > $b)?$a : $b;               
+    ?>
+</body>
+</html>
